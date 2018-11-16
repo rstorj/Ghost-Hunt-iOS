8eb9f7164ad060444b740f513de0a14540b76a72
Andrew Palmer <andrewpalmer@u.boisestate.edu>
2018-11-16 13:16:51 -0700

    Initial Commit

---
+++ Ghost Hunt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ghost Hunt
+//
+//  Created by Andrew Palmer on 11/16/18.
+//  Copyright © 2018 Andrew Palmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
